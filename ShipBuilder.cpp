a0e1d27ff7469062f84bf5c355d479d6633c1565
Martyna-Tu <martyna.tucholska.wkr@gmail.com>
2021-11-05 19:01:22 +0100

    renamed file and implemented functions in ShipBuilder.cpp

--- ShipBuilder.cpp
+++ ShipBuilder.cpp
@@ -1,10 +1,10 @@
 #include "ShipBuilder.hpp"
 
 ShipFactorySettingsBuilder ShipBuilder::createShipModel() {
-
+    return ShipFactorySettingsBuilder(ship_);
 }
 ShipOwnSettingsBuilder ShipBuilder::createShipDetails() {
-
+    return ShipOwnSettingsBuilder(ship_);
 }
 
 
